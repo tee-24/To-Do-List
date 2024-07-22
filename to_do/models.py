e476b60912132ded85fc8571f863b9072f7fb445
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-07-22 16:36:17 +0000

    Create the Task model

--- to_do/models.py
+++ to_do/models.py
@@ -1,4 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)   
+
+    class Meta:
+        ordering = ["complete"]
+    
+    def __str__(self):
+        return self.title
--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -83,6 +83,13 @@ DATABASES = {
 }
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com",
+    "https://*.codeinstitute-ide.net"
+]
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
