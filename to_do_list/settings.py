b2cb200d62a48f5b3559b9542ee1a9922a08968e
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-07-23 05:48:47 +0000

    Create and add a secret key

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-51r1$@s++*w3j9scic3aj%#fr_zahc_x9fw5i%(sa)$^d7si2n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
